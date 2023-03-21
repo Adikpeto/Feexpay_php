4c9cc4730e4af0dc8e849b8ef88d0a8f2c4d5e37
Adikpeto <adikpetoaristidezeus@gmail.com>
2023-03-21 10:34:35 +0100

    Create Function init

--- src/Feexpay.php
+++ src/Feexpay.php
@@ -9,10 +9,21 @@ class Feexpay
     /**
      * Create a new Skeleton Instance
      */
-    public function __construct(string $id)
+    public function __construct(string $id,int $amount,string $token,string $callback_url,string $mode)
     {
         // constructor body
+        $this->id = $id;
+        $this->amount = $amount;
+        $this->token = $token;
+        $this->callback_url = $callback_url;
+        $this->mode = $mode;
+
     
+    }
+
+    public function init()
+    {
+        return $this->token;
     }
 
   
