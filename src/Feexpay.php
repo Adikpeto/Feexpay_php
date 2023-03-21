d9caad51b277150575a92371ad5af4cd03e6d1fc
Adikpeto <adikpetoaristidezeus@gmail.com>
2023-03-21 08:30:23 +0100

    Upload all Files

---
+++ src/Feexpay.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Adikpeto\Feexpay;
+
+class Feexpay
+{
+    /**
+     * Create a new Skeleton Instance
+     */
+    public function __construct()
+    {
+        // constructor body
+    }
+
+  
+
+}
